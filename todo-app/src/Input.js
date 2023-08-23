55d0b53310b8e65562ed14c2909fd53cb1664ab1
Jason <jason5donoghue@gmail.com>
2023-08-23 20:59:24 +0100

    added enter task component

---
+++ todo-app/src/Input.js
@@ -0,0 +1,8 @@
+export default function Input() {
+    return (
+        <div>
+            <label for="newtask">New Task: </label>
+            <input type="text" id="newtask" name="newtask"/>
+        </div>
+    )
+}
